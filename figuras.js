5fd88f07c938796538ce4e24d7b92cb7235bd064
Jesus Cortacero <hola@jesuscorta.es>
2022-07-14 14:47:22 +0200

    Datos Circulo

--- figuras.js
+++ figuras.js
@@ -33,3 +33,29 @@ const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
 console.log("El area del triangulo es: " + areaTriangulo + "cm2");
 
 console.groupEnd();
+
+// Círculo
+console.group("Circulo");
+
+// Radio
+const radioCirculo = 4;
+console.log("El radio del circulo es: " + radioCirculo + "cm");
+
+// Diametro
+const diametroCirculo = radioCirculo * 2;
+console.log("El diametro del circulo es: " + diametroCirculo + "cm");
+
+// PI
+const PI = Math.PI;
+console.log("PI es : " + PI);
+
+// Circunferencia
+const perimetroCirculo = diametroCirculo * PI;
+console.log("La circunferencia del circulo es: " + perimetroCirculo + "cm");
+
+// Area
+
+const areaCirculo = (radioCirculo * radioCirculo) * PI;
+console.log("El area del circulo es: " + areaCirculo + "cm2");
+
+console.groupEnd();
