5d7f4046e176729f5c8a695f564e74168eee1272
Jesus Cortacero <hola@jesuscorta.es>
2022-07-15 09:29:26 +0200

    HTML cuadrado

--- figuras.js
+++ figuras.js
@@ -52,3 +52,24 @@ function areaCirculo(radio){
 }
 
 console.groupEnd();
+
+
+
+// Funciones html
+
+// CUADRADO
+
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+    const area = areaCuadrado(value);
+    alert(area)
+}
+
